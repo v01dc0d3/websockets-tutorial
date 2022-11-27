003adaf13f1e43a40aefae8d8c55a578cd8c1545
v01dc0d3 <voidcode@poliupg.ac.id>
2022-11-27 14:17:22 +0800

    Configure WebSocekt server address.

--- main.js
+++ main.js
@@ -71,12 +71,22 @@ function sendMoves(board, websocket) {
   });
 }
 
+function getWebSocketServer() {
+  if (window.location.host === "aaugustin.github.io") {
+    return "wss://websockets-tutorial.herokuapp.com/";
+  } else if (window.location.host === "localhost:8000") {
+    return "ws://localhost:8001/";
+  } else {
+    throw new Error(`Unsupported host: ${window.location.host}`);
+  }
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   // Initialize the UI.
   const board = document.querySelector(".board");
   createBoard(board);
   // Open the WebSocket connection and register event handlers.
-  const websocket = new WebSocket("ws://localhost:8001/");
+  const websocket = new WebSocket(getWebSocketServer());
   initGame(websocket);
   receiveMoves(board, websocket);
   sendMoves(board, websocket);
